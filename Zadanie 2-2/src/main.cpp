2b0dc6b75ad53e2b6fcd1d0a2999285e2a4979d9
Maciej <maciej.osiak@student.wat.edu.pl>
2020-11-30 09:09:56 +0100

    Wykonano zadanie 2-2-1 - Pojedyncza dioda LED D13

--- Zadanie 2-2/src/main.cpp
+++ Zadanie 2-2/src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
+int main()
+{
+  uint32_t i;
+  
+  DDRB |=(1 << 5); //pinMode(13, OUTPUT);
+  while (1)
+  {
+    PORTB |=(1 << 5); //digitalWrite(13, HIGH);
+    i=0x3FFFF;
+    do
+    {
+      __asm__ __volatile__("nop");
+    } while (i--);
+    PORTB &= !(1<<5); //digitalWrite(13, LOW);
+    i=0x3FFFF;
+    do
+    {
+      __asm__ __volatile__("nop");
+    } while (i--);
+  }
 }
